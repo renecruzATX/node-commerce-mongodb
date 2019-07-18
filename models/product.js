cb55876c38538dd9447b0c32e7ce18b43246147c
Rene Cruz <renecruzATX@outlook.com>
2019-07-18 15:26:24 -0500

    Mongodb logic for editing and saving products

--- models/product.js
+++ models/product.js
@@ -1,18 +1,26 @@
-const ObjectId = require('mongodb').ObjectID
+const ObjectId = require('mongodb').ObjectID;
 const getDb= require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, _id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this. imageUrl = imageUrl;
+    this._id = ObjectId(_id);
   }
 
   save() {
     const db = getDb();
-    return db.collection('products')
-      .insertOne(this)
+    let dbOp;
+
+    if (this._id) {
+      //Update the product
+      dbOp = db.collection('products').updateOne({_id: this._id}, {$set: this});
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
