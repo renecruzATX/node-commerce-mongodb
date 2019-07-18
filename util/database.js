d221864d4a87ed2fbc96bb755af54e6ab57d167e
Rene Cruz <renecruzATX@outlook.com>
2019-07-18 11:21:47 -0500

    Removed old sequelize code to establish connections to mongodb

--- util/database.js
+++ util/database.js
@@ -1,13 +1,24 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect('mongodb://tester:test1234@ds351987.mlab.com:51987/node-commerce-mongodb')
   .then(client => {
     console.log('Connected!');
-    callback(client);
+    _db = client.db();
+    callback();
   })
   .catch(err => console.log(err));
 };
 
-module.exports = mongoConnect;
+const getDb = ()  => {
+  if (_db) {
+    return _db;
+  }
+  throw 'no db found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
