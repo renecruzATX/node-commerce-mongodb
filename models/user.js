77044682bacd1583e65906195efb5c4c907e41fe
Rene Cruz <renecruzATX@outlook.com>
2019-07-22 16:59:19 -0500

    Created a User object for use with mongodb

--- models/user.js
+++ models/user.js
@@ -1,15 +1,22 @@
-const Sequelize = require('sequelize').Sequelize;
-const sequelize = require('../util/database');
+const getDb = require ('../util/database');
+const ObjectId = require('mongodb').ObjectID;
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-}); 
+class User {
+    constructor(username, email) {
+        this.name = username;
+        this.email = email;
+    }
+
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users').findOne({_id: ObjectId(userId)});
+    }
+
+}
 
 module.exports = User;
