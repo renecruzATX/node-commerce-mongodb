115634d4a2b2511f8d6d48abdd698a59e109599e
Rene Cruz <renecruzATX@outlook.com>
2019-07-25 15:18:12 -0500

    Functionality for displaying items in cart and redirecting to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,16 +44,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
     req.user
         .getCart()
-        .then(cart => {
-            return cart.getProducts()
-                .then(products => {
-                    res.render('shop/cart', {
-                        path: '/cart',
-                        pageTitle: 'Shopping Cart',
-                        products: products
-                    });
-                })
-                .catch(err => console.log(err));
+        .then(products => {            
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Shopping Cart',
+                products: products
+            });                  
         })
         .catch(err => console.log(err));    
 };
@@ -62,10 +58,11 @@ exports.postCart = (req, res, next) => {
     const prodId = req.body.productId; 
     Product.findById(prodId)
         .then(product => {
-            return req.user.addToCart(product);
+            return req.user.addToCart(product);            
         })
         .then(result => {
             console.log(result);
+            res.redirect('/cart');
         });
 
     // let fetchedCart;
